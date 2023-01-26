5779315366a3b38d40b47b791fbe35f2fcf1809e
feng.dev <feng@vitruvio.cl>
2023-01-26 04:51:02 -0300

    ok

--- savethedodo/next.config.js
+++ savethedodo/next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true
   },
-  basePath: "/PUBLIC-SAVETHEDODO-FAKE-ORG",
+  basePath: "",
+  assetPrefix: isProd ? 'https://fengdotdev.github.io/PUBLIC-SAVETHEDODO-FAKE-ORG/' : undefined,
 };
 
 module.exports = nextConfig;
